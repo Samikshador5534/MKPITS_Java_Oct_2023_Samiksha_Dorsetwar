c97a402712e2f6e44a0d696096c38cd5efe2e021
Samiksha Dorsetwar <paidlewarsamiksha@gmail.com>
2023-11-01 14:44:09 +0530

    code

---
+++ cprog6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int num1=0;
+	printf("enter a no.");
+	scanf("%d",&num1);
+	if(num1%2==0)
+	{
+    	printf(" even no ");
+	}
+	else
+	    printf(" odd no  ");
+	   
+}
